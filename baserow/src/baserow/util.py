36377cfcca977cc0e838b8a87ad2c72a67c34045
Tan Li <tan@tanli.dev>
2022-08-15 16:42:16 -0700

    update

--- baserow/src/baserow/util.py
+++ baserow/src/baserow/util.py
@@ -2,6 +2,7 @@ import os
 from typing import Dict
 import requests
 import json
+from urllib.parse import urljoin
 
 class BaserowConnect(object):
     def __init__(self, url: str) -> None:
@@ -10,16 +11,19 @@ class BaserowConnect(object):
 
     def __enter__(self) -> None:
         os.environ.setdefault("BASEROW_JWT", "")
-        url = f"{self.base_url}/api/user/token-auth/"
+        url = urljoin(self.base_url, "/api/user/token-auth/")
         res = requests.post(url, json=load_cert())
-        if res.status_code != 200:
+        if res.status_code != 201:
             raise Exception(f"Failed to authenticate baserow")
         user_token = json.loads(res.text)["token"]
         os.environ['BASEROW_JWT'] = user_token
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # make sure the dbconnection gets closed
-        print("Exit Baserow Connection")
+        # reset BASEROW_JWT for safety
+        os.environ.setdefault("BASEROW_JWT", "")
 
 def load_cert() -> Dict:
-    return {"username": "", "password": ""}
+    return {
+        "username": "",
+        "password": ""
+    }
