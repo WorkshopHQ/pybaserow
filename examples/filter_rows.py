363cb2ac97f82b78a9e2a3547c9ff43a8fae2f77
Tan Li <tan@tanli.dev>
2022-08-15 15:49:49 -0700

    update

--- examples/filter_rows.py
+++ examples/filter_rows.py
@@ -1,11 +1,10 @@
 from baserow.util import BaserowConnect
 from baserow.table import fetch
 
+def is_dataops_row(row):
+    return "Topic" in row and row["Topic"] == "dataops"
 
 with BaserowConnect("http://baserow.hub/"):
-    def is_dataops_row(row):
-        return "Topic" in row and row["Topic"] == "dataops"
-
     table_id = 285
     table = fetch(table_id)
     dataops_rows = table.get_rows(is_dataops_row)
