5b08f716e2a60bae979c0f4a486fca638e054c1c
Tan Li <tan@tanli.dev>
2022-08-15 17:31:50 -0700

    update

--- baserow/src/baserow/table.py
+++ baserow/src/baserow/table.py
@@ -1,15 +1,14 @@
 import os
-from typing import Callable
+from typing import Callable, Iterable
 
 class Table(object):
-    def __init__(self, id: int) -> None:
+    def __init__(self, id: int, jwt_token: str) -> None:
         super().__init__()
         self.id = id
+        self.jwt_token = jwt_token
 
-    def get_rows(self, filter: Callable = None):
+    def get_rows(self, filter: Callable = None) -> Iterable:
         return []
 
 def fetch(table_id) -> Table:
-    print(f"table id {table_id}")
-    print(os.environ['BASEROW_JWT'])
-    return Table(table_id)
+    return Table(table_id, os.environ['BASEROW_JWT'])
