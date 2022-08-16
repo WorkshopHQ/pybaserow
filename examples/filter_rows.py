e257ae5378defb90ec55e816b753ef761cd31a98
Tan Li <tan@tanli.dev>
2022-08-15 18:46:32 -0700

    update

--- examples/filter_rows.py
+++ examples/filter_rows.py
@@ -1,12 +1,18 @@
 from baserow.util import BaserowConnect
 from baserow.table import fetch
 
-def is_dataops_row(row):
-    return "Topic" in row and row["Topic"] == "dataops"
-
 table_id = 285
+
+keys = set()
+row_del_ids = []
 
 with BaserowConnect("http://baserow.hub/"):
     table = fetch(table_id)
     rows = table.get_rows(get_all=True)
-    print(len(rows))
+    for row in rows:
+        row_key = row['Date'] + row['Url'] + row['Topic']
+        if row_key in keys:
+            row_del_ids.append(row['id'])
+        keys.add(row_key)
+    # delete duplicated rows
+    table.del_rows(row_del_ids)
